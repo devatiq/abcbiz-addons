c8cbbdac3ddc943cf323f49db72bbcbeb74f4eb3
Atiqur Rahman <atiqurrahman@Atiqurs-MacBook-Pro.local>
2024-01-09 19:46:07 +0600

    contact info widget completed

--- includes/widgets/ABCContactInfo/Main.php
+++ includes/widgets/ABCContactInfo/Main.php
@@ -77,6 +77,36 @@ class Main extends BaseWidget {
 			]
 		);
 
+		//Alignment
+		$this->add_responsive_control(
+				'abcbiz_elementor_contact_info_inline_align',
+				[
+					'label' => esc_html__( 'Content Alignment', 'abcbiz-multi'),
+					'type' => Controls_Manager::CHOOSE,
+					'default' => 'flex-start',
+					'options' => [
+						'flex-start'    => [
+							'title' => esc_html__( 'Left', 'abcbiz-multi' ),
+							'icon' => 'eicon-text-align-left',
+						],
+						'center' => [
+							'title' => esc_html__( 'Center', 'abcbiz-multi' ),
+							'icon' => 'eicon-text-align-center',
+						],
+						'flex-end' => [
+							'title' => esc_html__( 'Right', 'abcbiz-multi' ),
+							'icon' => 'eicon-text-align-right',
+						],
+					],				
+					'selectors' => [
+						'{{WRAPPER}} .abcbiz-contact-info-inline' => 'justify-content: {{VALUE}}',
+					],
+					'condition' => [
+						'abcbiz_elementor_contact_style_type' => 'inline',
+					],
+				]
+			);
+
 		$this->end_controls_section();//end settings
 
 		//Contact Info section
@@ -212,6 +242,15 @@ class Main extends BaseWidget {
 			]
 		);
 
+		//Vimeo
+		$this->add_control(
+			'abcbiz_elementor_contact_info_vimeo_url',
+			[
+				'label' => esc_html__( 'Vimeo:', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+			]
+		);
+
 		//Youtube
 		$this->add_control(
 			'abcbiz_elementor_contact_info_yt_url',
@@ -236,27 +275,269 @@ class Main extends BaseWidget {
 			]
 		);
 
-		$this->add_control(
-			'abcbiz_elementor_block_quote_border_color',
+		//Icon Size
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_info_icon_size',
 			[
-				'label' => esc_html__( 'Border Color', 'abcbiz-multi' ),
-				'type'  => Controls_Manager::COLOR,
-				'default' => '#59a818',
+				'label' => esc_html__( 'Icon Size', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => 10,
+						'max' => 100,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 20,
+				],
 				'selectors' => [
-					'{{WRAPPER}} .abcbiz-elementor-block-quote-area blockquote' => 'border-left-color: {{VALUE}}',
+					'{{WRAPPER}} .abcbiz-contact-info-area svg' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
 				],
 			]
 		);
 
+		//Icon Color
+		$this->add_control(
+			'abcbiz_elementor_contact_info_icon_size_color',
+			[
+				'label' => esc_html__( 'Icon Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#903cf2',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-area svg' => 'fill: {{VALUE}}',
+				],
+			]
+		);
+
+		//Info typography
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
 			[
-				'name' => 'abcbiz_elementor_block_quote_typography',
-				'label' => esc_html__( 'Typography', 'abcbiz-multi' ),
-				'selector' => '{{WRAPPER}} .abcbiz-elementor-block-quote-area blockquote',
+				'name' => 'abcbiz_elementor_contact_info_text_typography',
+				'label' => esc_html__( 'Text Typography', 'abcbiz-multi' ),
+				'selector' => '{{WRAPPER}} .abcbiz-contact-info-area',
 			]
 		);
-        $this->end_controls_section();
+
+		//Info Text Color
+		$this->add_control(
+			'abcbiz_elementor_contact_info_text_color',
+			[
+				'label' => esc_html__( 'Text Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#333333',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-area, {{WRAPPER}} .abcbiz-contact-info-area a' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		//Info Hover Color
+		$this->add_control(
+			'abcbiz_elementor_contact_info_text_hov_color',
+			[
+				'label' => esc_html__( 'Text Hover Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#333333',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-area a:hover' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		//Spacing Stack
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_info_item_spacing',
+			[
+				'label' => esc_html__( 'Info Bottom Spacing', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => -20,
+						'max' => 100,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 10,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-address, {{WRAPPER}} .abcbiz-contact-info-mobile, {{WRAPPER}} .abcbiz-contact-info-email' => 'margin-bottom: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		//Spacing Inline
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_info_item_inline_spacing',
+			[
+				'label' => esc_html__( 'Info Right Spacing', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => -20,
+						'max' => 100,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 10,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-inline .abcbiz-contact-info-address, {{WRAPPER}} .abcbiz-contact-info-inline .abcbiz-contact-info-mobile, {{WRAPPER}} .abcbiz-contact-info-inline .abcbiz-contact-info-email' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
+				'condition' => [
+					'abcbiz_elementor_contact_style_type' => 'inline',
+				],
+			]
+		);
+
+        $this->end_controls_section();//end contact info style
+
+		//Social Icons Style
+		$this->start_controls_section(
+			'abcbiz_elementor_contact_social_icons_style',
+			[
+				'label' => esc_html__( 'Social Icons Style', 'abcbiz-multi' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+				'condition' => [
+					'abcbiz_elementor_contact_social_icon_switch' => 'yes',
+				],
+			]
+		);
+
+		//Icon Color
+		$this->add_control(
+			'abcbiz_elementor_contact_social_icon_color',
+			[
+				'label' => esc_html__( 'Icon Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#333333',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li svg' => 'fill: {{VALUE}}',
+				],
+			]
+		);
+
+		//Icon BG Color
+		$this->add_control(
+			'abcbiz_elementor_contact_social_icon_bg_color',
+			[
+				'label' => esc_html__( 'Icon BG Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#f0f0f0',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li a' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		//Icon Hover Color
+		$this->add_control(
+			'abcbiz_elementor_contact_social_icon_hov_color',
+			[
+				'label' => esc_html__( 'Icon Hover Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#333333',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li:hover svg' => 'fill: {{VALUE}}',
+				],
+			]
+		);
+
+		//Icon BG Color
+		$this->add_control(
+			'abcbiz_elementor_contact_social_icon_bg_hov_color',
+			[
+				'label' => esc_html__( 'Icon Hover BG Color', 'abcbiz-multi' ),
+				'type'  => Controls_Manager::COLOR,
+				'default' => '#dddddd',
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li a:hover' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		//Icon Size
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_social_icon_size',
+			[
+				'label' => esc_html__( 'Icon Size', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => 5,
+						'max' => 100,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 20,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li svg' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		//Icon BG Size
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_social_icon_bg_size',
+			[
+				'label' => esc_html__( 'Icon Background Size', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => 10,
+						'max' => 200,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 40,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li a' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		//Icon Gap
+		$this->add_responsive_control(
+			'abcbiz_elementor_contact_social_icon_gap',
+			[
+				'label' => esc_html__( 'Icon Gap', 'abcbiz-multi' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px'],
+				'range' => [
+					'px' => [
+						'min' => 1,
+						'max' => 50,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 10,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-contact-info-social-icons ul li' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();//end social icons style
 
     }
 
--- includes/widgets/ABCContactInfo/stack-render.php
+++ includes/widgets/ABCContactInfo/stack-render.php
@@ -22,48 +22,4 @@
 </div><!-- /end abcbiz contact social area -->
 
 
-	<style>
-    .abcbiz-contact-info-address,
-    .abcbiz-contact-info-mobile,
-    .abcbiz-contact-info-email {
-        display: flex;
-        align-items: center;
-        margin-bottom: 10px;
-    }
-
-    .abcbiz-contact-info-address svg,
-    .abcbiz-contact-info-mobile svg,
-    .abcbiz-contact-info-email svg {
-        flex-shrink: 0;
-        width: 30px;
-        height: 30px;
-        margin-right: 10px;
-    }
-
-    .abcbiz-contact-info-address span,
-    .abcbiz-contact-info-mobile span,
-    .abcbiz-contact-info-email span {
-        display: block;
-    }
-
-    .abcbiz-contact-info-mobile a,
-    .abcbiz-contact-info-email a {
-        text-decoration: none;
-        color: black; /* Change the color as needed */
-    }
-
-    .abcbiz-contact-info-social-icons ul{
-        margin: 0;
-        padding: 0;
-        list-style: none;
-    }
-
-    .abcbiz-contact-info-social-icons ul li svg{
-        width: 30px;
-        height: 30px;
-    }
-</style>
-
-
-
 	
