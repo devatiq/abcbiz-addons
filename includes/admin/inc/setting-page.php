38d3e7cbb05563beeb4c87496e689668829771fc
hmbashar <hmbashar@gmail.com>
2024-01-14 19:22:03 +0600

    added new button

--- includes/admin/inc/setting-page.php
+++ includes/admin/inc/setting-page.php
@@ -29,10 +29,15 @@ function abcbiz_settings_page() {
                 <h1><?php echo esc_html__("Welcome to", "abcbiz-multi"); ?> <?php echo esc_html($plugin_name); ?></h1>  
                 <p class="abcbiz-banner-version"><?php echo esc_html__("Version: ", "abcbiz-multi"); ?> <?php echo esc_html($plugin_version); ?></p>              
                 <!-- Buttons -->
-                <div class="abcbiz-resource-buttons">
-                    <a href="https://abcplugin.com/abcbiz-multi-addons-for-elementor/" target="_blank" class="button button-secondary"><?php echo esc_html__("Demo", "abcbiz-multi"); ?></a>
-                    <a href="https://abcplugin.com/" class="button button-secondary" target="_blank"><?php echo esc_html__("Documentation", "abcbiz-multi"); ?></a>
-                    <a href="https://abcplugin.com/contact-us/" target="_blank" class="button button-secondary"><?php echo esc_html__("Support", "abcbiz-multi"); ?></a>
+                <div class="abcbiz-resource-buttons">                
+                                        
+                    <a href="https://abcplugin.com/abcbiz-multi-addons-for-elementor/" target="_blank" class="button"><?php echo esc_html__("Demo", "abcbiz-multi"); ?></a>
+                    
+                    <a href="https://abcplugin.com/" class="button" target="_blank"><?php echo esc_html__("Documentation", "abcbiz-multi"); ?></a>
+                   
+                    <a href="https://abcplugin.com/contact-us/" target="_blank" class="button"><?php echo esc_html__("Support", "abcbiz-multi"); ?></a>     
+                    
+                    <a href="<?php echo esc_url(admin_url( 'admin.php?page=abcbiz_widgets_menu' )); ?>" class="button"><?php echo esc_html__( 'Avilable Widgets', 'abcbiz-multi' ); ?><span class="dashicons dashicons-arrow-right-alt"></span></a>
                 </div>
             </div>         
         </div>
