d9755dbb706aa579d2743e27cbfc4b6511502878
Atiqur Rahman <atiqurrahman@Atiqurs-MacBook-Pro.local>
2024-03-20 05:07:18 +0600

    Site Logo widget added

--- includes/widgets/ABCSiteLogo/Main.php
+++ includes/widgets/ABCSiteLogo/Main.php
@@ -29,7 +29,168 @@ class Main extends BaseWidget {
             ]
         );
 
+        //Custom Logo Switch
+        $this->add_control(
+			'abcbiz-elementor-site-logo-custom-switch',
+			[
+				'label' => esc_html__( 'Custom Logo', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::SWITCHER,
+				'custom_on' => esc_html__( 'Yes', 'abcbiz-addons' ),
+				'custom_off' => esc_html__( 'No', 'abcbiz-addons' ),
+				'return_value' => 'yes',
+				'default' => 'custom_off',
+			]
+		);
+
+        //Custom Logo
+        $this->add_control(
+			'abcbiz-elementor-site-logo-custom-image',
+			[
+				'label' => esc_html__( 'Choose Logo', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
+                'condition' => [
+                    'abcbiz-elementor-site-logo-custom-switch' => 'yes'
+                ],
+			]
+		);
+
+        //URL
+        $this->add_control(
+			'abcbiz-elementor-site-logo-link',
+			[
+				'label' => esc_html__( 'Link', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => false,
+				],
+				'label_block' => true,
+			]
+		);
+
+        //Alignment
+		$this->add_responsive_control(
+			'abcbiz-elementor-site-logo-align',
+			[
+				'label' => esc_html__( 'Alignment', 'abcbiz-addons'),
+				'type' => Controls_Manager::CHOOSE,
+				'default' => 'left',
+				'options' => [
+					'left'    => [
+						'title' => esc_html__( 'Left', 'abcbiz-addons' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'abcbiz-addons' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'abcbiz-addons' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],				
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-elementor-site-logo-area' => 'text-align: {{VALUE}}',
+				],
+			]
+		);
+
         $this->end_controls_section();//end content
+
+        //Style Tab
+        $this->start_controls_section(
+            'abcbiz-elementor-site-logo-style',
+            [
+                'label' => esc_html__( 'Logo Style', 'abcbiz-addons' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        //width
+        $this->add_control(
+			'abcbiz-elementor-site-logo-width',
+			[
+				'label' => esc_html__( 'Logo Width', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%'],
+				'range' => [
+					'px' => [
+						'min' => 10,
+						'max' => 1000,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'default' => [
+					'unit' => '%',
+					'size' => 80,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-elementor-site-logo-area img' => 'width: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+        //Spacing
+        $this->add_control(
+			'abcbiz-elementor-site-logo-spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => ['px'],
+				'default' => [
+					'top' => 0,
+					'right' => 0,
+					'bottom' => 0,
+					'left' => 0,
+					'unit' => 'px',
+					'isLinked' => true,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-elementor-site-logo-area img' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+        //Border
+        $this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'abcbiz-elementor-site-logo-border',
+                'label' => esc_html__( 'Logo Border', 'abcbiz-addons' ),
+				'selector' => '{{WRAPPER}} .abcbiz-elementor-site-logo-area img',
+			]
+		);
+
+         //Border Radius
+         $this->add_control(
+			'abcbiz-elementor-site-logo-border-radius',
+			[
+				'label' => esc_html__( 'Border Radius', 'abcbiz-addons' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => ['px'],
+				'default' => [
+					'top' => 0,
+					'right' => 0,
+					'bottom' => 0,
+					'left' => 0,
+					'unit' => 'px',
+					'isLinked' => true,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .abcbiz-elementor-site-logo-area img' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+        $this->end_controls_section();//end style
     }
 
     protected function render() {      
--- includes/widgets/ABCSiteLogo/renderview.php
+++ includes/widgets/ABCSiteLogo/renderview.php
@@ -6,8 +6,36 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
-    ?>
 
-    <div class="abcbiz-elementor-site-logo-area">
-        
-    </div>
+$abcbiz_settings = $this->get_settings_for_display();
+
+// Get the site title for alt text.
+$abcbiz_site_title = get_bloginfo('name');
+
+// Define a default logo URL.
+$abcbiz_default_logo_url = plugins_url(trim(str_replace(WP_PLUGIN_DIR, '', ABCBIZ_Path), '/') . '/assets/img/abcbiz-addons-logo.svg');
+
+if ('yes' === $abcbiz_settings['abcbiz-elementor-site-logo-custom-switch'] && !empty($abcbiz_settings['abcbiz-elementor-site-logo-custom-image']['url'])) {
+    $abcbiz_logo_url = $abcbiz_settings['abcbiz-elementor-site-logo-custom-image']['url'];
+} else {
+    // Fallback to the site logo set in the WordPress Customizer if available.
+    $abcbiz_custom_logo_id = get_theme_mod('custom_logo');
+    $abcbiz_logo_url = $abcbiz_custom_logo_id ? wp_get_attachment_image_url($abcbiz_custom_logo_id, 'full') : $abcbiz_default_logo_url;
+}
+
+// Ensure the logo URL is not empty; if it is, use the default logo.
+$abcbiz_logo_url = !empty($abcbiz_logo_url) ? $abcbiz_logo_url : $abcbiz_default_logo_url;
+
+// Check if a link is provided and not just the default '#'.
+$abcbiz_logo_link = !empty($abcbiz_settings['abcbiz-elementor-site-logo-link']['url']) && $abcbiz_settings['abcbiz-elementor-site-logo-link']['url'] != '#' ? $abcbiz_settings['abcbiz-elementor-site-logo-link']['url'] : '';
+
+?>
+<div class="abcbiz-elementor-site-logo-area">
+    <?php if (!empty($abcbiz_logo_link)): ?>
+        <a href="<?php echo esc_url($abcbiz_logo_link); ?>" <?php echo $abcbiz_settings['abcbiz-elementor-site-logo-link']['is_external'] ? 'target="_blank"' : ''; ?> <?php echo $abcbiz_settings['abcbiz-elementor-site-logo-link']['nofollow'] ? 'rel="nofollow"' : ''; ?>>
+            <img src="<?php echo esc_url($abcbiz_logo_url); ?>" alt="<?php echo esc_attr($abcbiz_site_title); ?>">
+        </a>
+    <?php else: ?>
+        <img src="<?php echo esc_url($abcbiz_logo_url); ?>" alt="<?php echo esc_attr($abcbiz_site_title); ?>">
+    <?php endif; ?>
+</div>
