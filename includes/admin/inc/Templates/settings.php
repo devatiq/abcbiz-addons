71b3c2e998130e9ba887a0ff67e452b6e1cd7d63
Md Abul Bashar <hmbashar@users.noreply.github.com>
2024-08-18 18:22:01 +0600

    deprecated error fix for strpos() and str_replace()

--- includes/admin/inc/Templates/settings.php
+++ includes/admin/inc/Templates/settings.php
@@ -1,31 +1,34 @@
 <?php
 namespace ABCBizAddons\includes\admin\inc\Templates;
 
-// Exit if accessed directly
 if (!defined('ABSPATH')) {
     exit;
 }
-class TemplatesAdminMenu  {
+
+class TemplatesAdminMenu {
     /**
      * Constructor to initialize the hook.
      */
     public function __construct() {
-        add_action('admin_menu', array($this, 'addSubMenu'));
-        add_action('admin_enqueue_scripts', array($this, 'enqueueAdminStyles'));
+        add_action('admin_menu', [$this, 'addSubMenu']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueueAdminStyles']);
     }
 
     /**
      * Adds a submenu under the specified parent menu.
      */
     public function addSubMenu() {
-        add_submenu_page(
+        $hook_suffix = add_submenu_page(
             'abcbiz_home',          // Parent slug
             esc_html__('Templates', 'abcbiz-addons'), // Page title
             esc_html__('Templates', 'abcbiz-addons'), // Menu title
             'manage_options',       // Capability
             'abcbiz_templates',     // Menu slug
-            array($this, 'templatesPage') // Callback function
+            [$this, 'templatesPage'] // Callback function
         );
+
+        // Now hook the style enqueue function to this specific page load.
+        add_action('admin_print_styles-' . $hook_suffix, [$this, 'enqueueAdminStyles']);
     }
 
     /**
@@ -37,17 +40,10 @@ class TemplatesAdminMenu  {
         $markup->DisplayMarkup();
     }
 
-     /**
+    /**
      * Enqueue styles specifically for this page.
      */
-    public function enqueueAdminStyles($hook) {
-        // Obtain the hook suffix when the submenu is added.
-        $page_hook_suffix = add_submenu_page(null, '', '', 'manage_options', 'abcbiz_templates');
-
-        if ($hook !== $page_hook_suffix) {
-            return;
-        }
-
+    public function enqueueAdminStyles() {
         wp_enqueue_style('abcbiz-templates-admin-css', ABCBIZ_Admin_CSS . '/admin-templates.css');
     }
 }
