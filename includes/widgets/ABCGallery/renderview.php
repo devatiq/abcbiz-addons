bd9a842d1b04f004e7e698bf176c1dcfcd8666cf
hmbashar <hmbashar@gmail.com>
2024-08-22 12:53:02 +0600

    zoom effect added

--- includes/widgets/ABCGallery/renderview.php
+++ includes/widgets/ABCGallery/renderview.php
@@ -2,45 +2,63 @@
 /**
  * Render View for ABC Animated Text
  */
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly
 
 $settings = $this->get_settings_for_display();
-		foreach ( $settings['abcbiz_elementor_gallery'] as $image ) {
-			//echo '<img src="' . esc_attr( $image['url'] ) . '">';
-		}
+foreach ($settings['abcbiz_elementor_gallery'] as $image) {
+	//echo '<img src="' . esc_attr( $image['url'] ) . '">';
+}
 
 ?>
 
 <div class="adasdfsdfpopup-gallery">
-	<span href="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_b.jpg" title="The Cleaner"><img src="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_b.jpg" title="Winter Dance"><img src="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg" title="The Uninvited Guest"><img src="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_b.jpg" title="Oh no, not again!"><img src="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_b.jpg" title="Swan Lake"><img src="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg" title="The Shake"><img src="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_b.jpg" title="Who's that, mommy?"><img src="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_b.jpg" title="The Cleaner"><img
+			src="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_b.jpg" title="Winter Dance"><img
+			src="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg" title="The Uninvited Guest"><img
+			src="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_b.jpg" title="Oh no, not again!"><img
+			src="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_b.jpg" title="Swan Lake"><img
+			src="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg" title="The Shake"><img
+			src="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_s.jpg" width="75" height="75"></span>
+	<span href="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_b.jpg" title="Who's that, mommy?"><img
+			src="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_s.jpg" width="75" height="75"></span>
 </div>
 
 <script>
-    (function($){
-        $(document).ready(function() {
-	$('.adasdfsdfpopup-gallery').magnificPopup({
-		delegate: 'span',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' by Marsel Van Oosten';
-			}
-		}
-	});
-});
-    })(jQuery);
+	(function ($) {
+		$(document).ready(function () {
+
+			$('.adasdfsdfpopup-gallery').magnificPopup({
+				delegate: 'span',
+				type: 'image',
+				closeOnContentClick: false,
+				closeBtnInside: false,
+				mainClass: 'mfp-with-zoom mfp-img-mobile',
+				allowHTMLInTemplate: true,
+				image: {
+					verticalFit: true,
+					titleSrc: function (item) {
+						return item.el.attr('title');
+					}
+				},
+
+				gallery: {
+					enabled: true
+				},
+				zoom: {
+					enabled: true,
+					duration: 300, // don't foget to change the duration also in CSS
+					opener: function (element) {
+						return element.find('img');
+					}
+				}
+
+			});
+		});
+	})(jQuery);
 </script>
