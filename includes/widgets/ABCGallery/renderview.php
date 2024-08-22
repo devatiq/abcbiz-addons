cd4df69a1c6f73088e1467df2f5eaaa6dcb839e1
hmbashar <hmbashar@gmail.com>
2024-08-22 14:16:39 +0600

    gallary dynamic

--- includes/widgets/ABCGallery/renderview.php
+++ includes/widgets/ABCGallery/renderview.php
@@ -6,34 +6,33 @@ if (!defined('ABSPATH'))
 	exit; // Exit if accessed directly
 
 $settings = $this->get_settings_for_display();
-foreach ($settings['abcbiz_elementor_gallery'] as $image) {
-	//echo '<img src="' . esc_attr( $image['url'] ) . '">';
-}
 
 ?>
 
-<div class="adasdfsdfpopup-gallery">
-	<span href="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_b.jpg" title="The Cleaner"><img
-			src="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_b.jpg" title="Winter Dance"><img
-			src="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg" title="The Uninvited Guest"><img
-			src="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_b.jpg" title="Oh no, not again!"><img
-			src="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_b.jpg" title="Swan Lake"><img
-			src="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg" title="The Shake"><img
-			src="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_s.jpg" width="75" height="75"></span>
-	<span href="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_b.jpg" title="Who's that, mommy?"><img
-			src="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_s.jpg" width="75" height="75"></span>
+<div class="abcbiz-photos-gallery">
+	<?php if ($settings['abcbiz_elementor_gallery']) { ?>
+		<?php foreach ($settings['abcbiz_elementor_gallery'] as $image) { 
+			  if(!empty(wp_get_attachment_caption($image['id']))) {
+				$caption = wp_get_attachment_caption($image['id']);				
+			  }elseif(!empty(get_post_field( 'post_title', $image['id'] ))) {
+				$caption = get_post_field( 'post_title', $image['id'] );
+			  }else{
+				$caption = '';
+			  }					
+			?>	
+			<span href="<?php echo esc_attr($image['url']); ?>" title="<?php echo esc_attr($caption); ?>"><img
+					src="<?php echo esc_attr($image['url']); ?>"></span>
+
+		<?php } ?>
+	<?php } ?>
+	
 </div>
 
 <script>
 	(function ($) {
 		$(document).ready(function () {
 
-			$('.adasdfsdfpopup-gallery').magnificPopup({
+			$('.abcbiz-photos-gallery').magnificPopup({
 				delegate: 'span',
 				type: 'image',
 				closeOnContentClick: false,
