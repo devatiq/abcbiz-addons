758b4b3a2ff16f6b700de283076a0eb0b6d29b0b
hmbashar <hmbashar@gmail.com>
2024-09-11 13:17:19 +0600

    style added

--- includes/widgets/ABCMailChimp/Main.php
+++ includes/widgets/ABCMailChimp/Main.php
@@ -163,7 +163,7 @@ class Main extends \Elementor\Widget_Base
         $settings = $this->get_settings_for_display();
         ?>
         <div class="abcbiz-mailchimp-wrapper">
-            <form id="abcbiz-mailchimp-form">
+            <form id="abcbiz-mailchimp-form" class="abcbiz-mailchimp-single-form">
                 <?php if ('yes' === $settings['enable_name_fields']): ?>
                     <input id="abcbiz-mailchimp-fname" type="text" name="fname" placeholder="First Name">
                     <input id="abcbiz-mailchimp-lname" type="text" name="lname" placeholder="Last Name">
@@ -171,7 +171,7 @@ class Main extends \Elementor\Widget_Base
                 <input id="abcbiz-mailchimp-email" type="email" name="email" placeholder="Your Email" required>
                 <input id="abcbiz-mailchimp-list" type="hidden" name="list"
                     value="<?php echo esc_attr($settings['mailchimp_list_id']); ?>">
-                <button type="submit"><?php esc_html_e('Subscribe', 'abcbiz-addons'); ?></button>
+                <button type="submit" id="abcbiz-mailchimp-submit"><?php esc_html_e('Subscribe', 'abcbiz-addons'); ?></button>
                 <div class="abcbiz-mailchimp-response"></div>
             </form>
         </div>
