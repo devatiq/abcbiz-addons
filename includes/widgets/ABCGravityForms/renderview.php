94a7d28620e10f037ddffa872749239f7729dc1b
hmbashar <hmbashar@gmail.com>
2024-09-12 17:53:17 +0600

    condition added for form rendering

--- includes/widgets/ABCGravityForms/renderview.php
+++ includes/widgets/ABCGravityForms/renderview.php
@@ -9,6 +9,13 @@ if (!defined('ABSPATH')) {
 $settings = $this->get_settings_for_display();
 $form_id = isset($settings['abcbiz_gf_form_id']) ? $settings['abcbiz_gf_form_id'] : '';
 
+// Get other form settings from Elementor controls
+$display_title = isset($settings['display_title']) && $settings['display_title'] === 'yes' ? true : false;
+$display_description = isset($settings['display_description']) && $settings['display_description'] === 'yes' ? true : false;
+$enable_ajax = isset($settings['enable_ajax']) && $settings['enable_ajax'] === 'yes' ? true : false;
+$field_values = isset($settings['field_values']) ? $settings['field_values'] : '';
+$tabindex = isset($settings['tabindex']) ? $settings['tabindex'] : 1;
+
 ?>
 
 <div class="abcbiz-gravity-form-wrapper">
@@ -17,8 +24,8 @@ $form_id = isset($settings['abcbiz_gf_form_id']) ? $settings['abcbiz_gf_form_id'
         // Check if the form exists and if it's active
         $form = \GFAPI::get_form($form_id);
         if ($form && !rgar($form, 'is_trash') && rgar($form, 'is_active')) {
-            // Render the form if it exists and is active
-            gravity_form($form_id, false, false, false, '', true, 1, true);
+            // Render the form with all customizable settings
+            gravity_form($form_id, $display_title, $display_description, false, $field_values, $enable_ajax, $tabindex, true);
         } else {
             // Show a notice if the form is not active or is trashed
             ?>
