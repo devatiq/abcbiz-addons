101410265f8adf9d3bd6d8f455b1b34b0996bee8
hmbashar <hmbashar@gmail.com>
2024-09-15 19:10:19 +0600

    swiper class added

--- includes/widgets/ABCSlider/RenderView.php
+++ includes/widgets/ABCSlider/RenderView.php
@@ -6,17 +6,17 @@ $loop = $settings['loop'] === 'yes' ? 'true' : 'false';
 $autoplay = $settings['autoplay'] === 'yes' ? 'true' : 'false';
 
 ?>
-<div class="swiper-container">
+<div class="swiper-container swiper">
     <div class="swiper-wrapper">
         <!-- Slides -->
         <div class="swiper-slide">Slide 1</div>
         <div class="swiper-slide">Slide 2</div>
         <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 3</div>
+        <div class="swiper-slide">Slide 4</div>
+        <div class="swiper-slide">Slide 5</div>
+        <div class="swiper-slide">Slide 6</div>
+        <div class="swiper-slide">Slide 7</div>
+        <div class="swiper-slide">Slide 8</div>
         <!-- Add more slides as needed -->
     </div>
     <!-- Add Pagination -->
