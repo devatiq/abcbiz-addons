b8126f35342af690f3d77659b4e68e362c710bb6
hmbashar <hmbashar@gmail.com>
2024-09-29 13:37:39 +0600

    markup with layout

--- includes/widgets/ABCModernPostGrid/renderview.php
+++ includes/widgets/ABCModernPostGrid/renderview.php
@@ -7,3 +7,80 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 $abcbiz_settings = $this->get_settings_for_display();
 ?>
 
+<!-- Modern Post Grid Area-->
+<div class="abcbiz-modren-posts-grid-area">
+    <div class="abcbiz-modren-posts-grid-wrapper">
+        <!-- Single Post -->
+        <div class="abcbiz-modren-single-post">
+            <!-- Post Thumbnail -->
+            <div class="abcbiz-modren-single-post-thumbanil">
+                <img src="<?php echo ABCBIZ_Assets; ?>/img/img-hover-placeholder.jpg" alt="">
+            </div><!--/ Post Thumbnail -->
+            
+            <!-- Post Contents -->
+            <div class="abcbiz-modren-single-post-contents">
+                <!-- Post Title -->
+                <div class="abcbiz-modren-single-post-title">
+                    <h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+                </div><!--/ Post Title -->
+
+                <!-- Post info -->
+                <div class="abcbiz-modren-single-post-info">
+                    <ul>
+                        <li><span class="fa fa-calendar"></span><a href="#">July 20, 2018</a></li>
+                        <li><span class="fa fa-user"></span><a href="#">Admin</a></li>
+                        <li><span class="fa fa-comments"></span><a href="#">2 Comments</a></li>
+                    </ul>
+                </div><!--/ Post info -->
+            </div><!-- Post Contents -->
+        </div><!--/ Single Post -->
+        <!-- Single Post -->
+        <div class="abcbiz-modren-single-post">
+            <!-- Post Thumbnail -->
+            <div class="abcbiz-modren-single-post-thumbanil">
+                <img src="<?php echo ABCBIZ_Assets; ?>/img/img-hover-placeholder.jpg" alt="">
+            </div><!--/ Post Thumbnail -->
+            
+            <!-- Post Contents -->
+            <div class="abcbiz-modren-single-post-contents">
+                <!-- Post Title -->
+                <div class="abcbiz-modren-single-post-title">
+                    <h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+                </div><!--/ Post Title -->
+
+                <!-- Post info -->
+                <div class="abcbiz-modren-single-post-info">
+                    <ul>
+                        <li><span class="fa fa-calendar"></span><a href="#">July 20, 2018</a></li>
+                        <li><span class="fa fa-user"></span><a href="#">Admin</a></li>
+                        <li><span class="fa fa-comments"></span><a href="#">2 Comments</a></li>
+                    </ul>
+                </div><!--/ Post info -->
+            </div><!-- Post Contents -->
+        </div><!--/ Single Post -->
+        <!-- Single Post -->
+        <div class="abcbiz-modren-single-post">
+            <!-- Post Thumbnail -->
+            <div class="abcbiz-modren-single-post-thumbanil">
+                <img src="<?php echo ABCBIZ_Assets; ?>/img/img-hover-placeholder.jpg" alt="">
+            </div><!--/ Post Thumbnail -->
+            
+            <!-- Post Contents -->
+            <div class="abcbiz-modren-single-post-contents">
+                <!-- Post Title -->
+                <div class="abcbiz-modren-single-post-title">
+                    <h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+                </div><!--/ Post Title -->
+
+                <!-- Post info -->
+                <div class="abcbiz-modren-single-post-info">
+                    <ul>
+                        <li><span class="fa fa-calendar"></span><a href="#">July 20, 2018</a></li>
+                        <li><span class="fa fa-user"></span><a href="#">Admin</a></li>
+                        <li><span class="fa fa-comments"></span><a href="#">2 Comments</a></li>
+                    </ul>
+                </div><!--/ Post info -->
+            </div><!-- Post Contents -->
+        </div><!--/ Single Post -->
+    </div>
+</div><!--/ Modern Post Grid Area-->
