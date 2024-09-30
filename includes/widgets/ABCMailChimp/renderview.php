4c9486552422372e7f2c583058c164776dbfd6d7
hmbashar <hmbashar@gmail.com>
2024-09-30 14:00:48 +0600

    mailchimp inline form style added

--- includes/widgets/ABCMailChimp/renderview.php
+++ includes/widgets/ABCMailChimp/renderview.php
@@ -10,7 +10,7 @@ $settings = $this->get_settings_for_display();
 ?>
 
 <div class="abcbiz-mailchimp-wrapper">
-    <form id="abcbiz-mailchimp-form" class="abcbiz-mailchimp-single-form">
+    <form id="abcbiz-mailchimp-form" class="abcbiz-mailchimp-single-form abcbiz-mailchimp-inline-forms">
         <?php if ('yes' === $settings['enable_name_fields']): ?>
             <input id="abcbiz-mailchimp-fname" type="text" name="fname" placeholder="First Name">
             <input id="abcbiz-mailchimp-lname" type="text" name="lname" placeholder="Last Name">
@@ -20,8 +20,11 @@ $settings = $this->get_settings_for_display();
             value="<?php echo esc_attr($settings['mailchimp_list_id']); ?>">
         <button type="submit"
             id="abcbiz-mailchimp-submit"><?php echo esc_html($settings['submit_button_text']); ?>
-           
-        </button>
+        <!-- <button type="submit"
+            id="abcbiz-mailchimp-inline-submit" class="abcbiz-mailchimp-inline-submit-texts">
+     <?php echo esc_html($settings['submit_button_text']); ?> 
+         <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" id="fi_12604076"><g clip-rule="evenodd" fill="rgb(0,0,0)" fill-rule="evenodd"><path d="m19 13.5c.2761 0 .5-.2239.5-.5v-8.5h-8.5c-.2761 0-.5.22386-.5.5s.2239.5.5.5h7.5v7.5c0 .2761.2239.5.5.5z"></path><path d="m4.64645 19.3536c.19526.1952.51184.1952.7071 0l14.00005-14.00005c.1952-.19526.1952-.51184 0-.7071-.1953-.19527-.5119-.19527-.7072 0l-13.99995 13.99995c-.19527.1953-.19527.5119 0 .7072z"></path></g></svg> 
+        </button> -->
     </form>
     <div class="abcbiz-mailchimp-response"></div>
 </div>
