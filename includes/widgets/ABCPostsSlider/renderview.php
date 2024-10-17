03e853cb6d857800a0aba2558cb0126b0c234c08
hmbashar <hmbashar@gmail.com>
2024-10-17 15:55:43 +0600

    nagivation style

--- includes/widgets/ABCPostsSlider/renderview.php
+++ includes/widgets/ABCPostsSlider/renderview.php
@@ -67,7 +67,9 @@ $posts = new WP_Query($arg);
                             <!-- Slider Contents -->
                             <div class="abcbiz-posts-slider-contents">
                                 <!-- Post Date -->
-                                <span class="abcbiz-posts-slider-date"><?php echo esc_html(get_the_date('d M Y')); ?></span>
+                                <span
+                                    class="abcbiz-posts-slider-date"><?php $this->post_date_icon();
+                                    echo esc_html(get_the_date('d M Y')); ?></span>
                                 <!-- Post Title -->
                                 <h3 class="abcbiz-posts-slider-title"><a
                                         href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -90,8 +92,29 @@ $posts = new WP_Query($arg);
 
     <!-- Add Navigation -->
     <div class="abcbiz-posts-slider-navigation">
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next">
+            <svg version="1.1" id="fi_664866" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.009 512.009"
+                style="enable-background:new 0 0 512.009 512.009;" xml:space="preserve">
+                <g>
+                    <path
+                        d="M508.625 247.801L508.625 247.801 392.262 131.437c-4.18-4.881-11.526-5.45-16.407-1.269-4.881 4.18-5.45 11.526-1.269 16.407.39.455.814.88 1.269 1.269l96.465 96.582H11.636C5.21 244.426 0 249.636 0 256.063s5.21 11.636 11.636 11.636H472.32l-96.465 96.465c-4.881 4.18-5.45 11.526-1.269 16.407s11.526 5.45 16.407 1.269c.455-.39.88-.814 1.269-1.269l116.364-116.364C513.137 259.67 513.137 252.34 508.625 247.801z">
+                    </path>
+                </g>
+            </svg>
+        </div>
+        <div class="swiper-button-prev">
+            <svg version="1.1" id="fi_545680" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+                style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                <g>
+                    <path
+                        d="M492 236H68.442l70.164-69.824c7.829-7.792 7.859-20.455.067-28.284-7.792-7.83-20.456-7.859-28.285-.068L5.884 242.824c-.007.006-.012.013-.018.019-7.809 7.792-7.834 20.496-.002 28.314.007.006.012.013.018.019l104.504 104c7.828 7.79 20.492 7.763 28.285-.068 7.792-7.829 7.762-20.492-.067-28.284L68.442 276H492c11.046 0 20-8.954 20-20 0-11.046-8.954-20-20-20z">
+                    </path>
+                </g>
+            </svg>
+
+        </div>
     </div><!--/ Add Navigation -->
 
 </div><!-- Posts Slider Area-->
